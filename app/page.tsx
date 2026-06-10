c3d5d002c1234cb5b6ef46da7f050c71e09dd009
Felix Heberle <fh@dealtactix.com>
2026-06-10 23:25:03 +0200

    Section-Reihenfolge: Method ('Die Methode') wieder vor VerstandKoerper

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function HomePage() {
         <Hero />
         <PositioningLine />
         <Erkennen />
-        <VerstandKoerper />
         <Method />
+        <VerstandKoerper />
         <Marisha />
         <Angebot />
         <Erfahrungen />
